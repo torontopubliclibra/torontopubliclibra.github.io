6e33221fa41a9a4733d679e36e520227c6c4ebd2
dana rosamund teagle (she/they) <95252661+torontopubliclibra@users.noreply.github.com>
2022-07-14 15:17:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,35 +4,35 @@ const app = {};
 // flickr api key
 app.key = `ba29e2524c03fb467cf6af47fca859df`;
 
-// random element function
+// flickr api address to call (search method, using api key, sorting by relevance, for 10 photos, in json)
+app.flickrAPI = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${app.key}&sort=relevance&per_page=10&format=json&dataType=json&nojsoncallback=1`;
+
+// random array element function
 app.getRandomElement = function(array){
     const index = Math.floor(Math.random() * array.length)
     return array[index]
 };
 
-// flickr api address to call
-app.flickrAPI = `https://api.flickr.com/services/rest/?method=flickr.photos.search&sort=relevance&format=json&nojsoncallback=1`;
-
 // cities array to randomize
-app.cities = [`new york city`, `toronto`, `chicago`, `london`, `glasgow`, `melbourne`, `paris`, `amsterdam`, `venice`, `mumbai`];
+app.cities = [`toronto`, `vancouver`, `new york city`, `chicago`, `boston`, `washington, d.c.`, `london`, `glasgow`, `melbourne`, `paris`, `amsterdam`, `stockholm`, `venice`, `mumbai`, `cairo`];
 
-// function to display photos
-app.displayPhotos = function(data) {
-    console.log(data);
-    // random whole number between 0 and 9
-    app.randomNumber = Math.floor(Math.random() * 10);
+// display photos function
+app.displayPhoto = function(data){
 
-    // photo ID, server ID, photo secret, and photo alt (using random 0-9 result) from data
-    let photoID = data.photos.photo[app.randomNumber].id;
-    let serverID = data.photos.photo[app.randomNumber].server;
-    let photoSecret = data.photos.photo[app.randomNumber].secret;
-    let photoAlt = data.photos.photo[app.randomNumber].title;
+    // generate random whole number between 0 and 9 (for the 10 photos in each object)
+    const randomNumber = Math.floor(Math.random() * 10);
+
+    // photo ID, server ID, photo secret, and photo alt (using random 0-9 result) from data object
+    const photoID = data.photos.photo[randomNumber].id;
+    const serverID = data.photos.photo[randomNumber].server;
+    const photoSecret = data.photos.photo[randomNumber].secret;
+    const photoAlt = data.photos.photo[randomNumber].title;
 
     // turn info into a url
-    let photoURL = `https://live.staticflickr.com/${serverID}/${photoID}_${photoSecret}.jpg`;
+    const photoURL = `https://live.staticflickr.com/${serverID}/${photoID}_${photoSecret}.jpg`;
 
     // HTML to add to page
-    let photoHTML = `<h3 class="city-label">${app.selectedCity}</h3><div class="image-container"><a href="${photoURL}" target="_blank"><img src="${photoURL}" alt="${photoAlt}" title="${photoAlt}" class="pigeon-image"></a></div>`;
+    const photoHTML = `<h3 class="city-label">${app.selectedCity}</h3><div class="image-container"><a href="${photoURL}" target="_blank"><img src="${photoURL}" alt="${photoAlt}" title="${photoAlt}" class="pigeon-image"></a></div>`;
 
     // image results section
     app.imageResults = $(`#image-results`);
@@ -40,60 +40,41 @@ app.displayPhotos = function(data) {
     // add HTML to the image results section
     app.imageResults.html(photoHTML);
 
-    // reset city selector
+    // reset city selection
     $(`#cities`).val(``);
+    app.selectedCity = ``;
 };
 
 // initialize the app
 app.init = function(){
         
-    // submit function on button click
-    $(`.submit`).on(`click`, function(event){
+    // run function on submit button click
+    $(`.submit`).on(`click`, function(){
 
-        // prevent default reload
-        event.preventDefault();
-
-        // city value from selection
+        // pull city value from selection
         app.selectedCity = $(`#cities`).val();
 
-        // flickr search query (pigeon `city`)
-        app.pigeonSearch = `pigeon ${app.selectedCity}`
+        // update flickr api search query using city value
+        app.flickrQuery = {text: `pigeon ${app.selectedCity}`,};
 
-        // flickr api search options for 10 results
-        app.flickrOptions = {
-            api_key: app.key,
-            text: app.pigeonSearch,
-            per_page: 10,
-        };
-
-        // run json using ajax shorthand (if selected city has a value)
+        // run json and display photo using ajax shorthand (so long as app.selectedCity has a value)
         if (app.selectedCity != null){
-            $.getJSON(app.flickrAPI, app.flickrOptions, app.displayPhotos);
+            $.getJSON(app.flickrAPI, app.flickrQuery, app.displayPhoto);
         };
     });
 
-    // randomize function on button click
+    // run function on random button click
     $(`.random`).on(`click`, function(){
 
-        // randomize city value
+        // randomize city value from cities array
         app.selectedCity = app.getRandomElement(app.cities);
 
-        // flickr search query (pigeon `city`)
-        app.pigeonSearch = `pigeon ${app.selectedCity}`;
+        // update flickr api search query using city value
+        app.flickrQuery = {text: `pigeon ${app.selectedCity}`,};
 
-        // flickr api search options for 10 results
-        app.flickrOptions = {
-            api_key: app.key,
-            text: app.pigeonSearch,
-            per_page: 10,
-        };
-
-        // run json using ajax shorthand (if selected city has a value)
-        if (app.selectedCity != null){
-            $.getJSON(app.flickrAPI, app.flickrOptions, app.displayPhotos);
-        };
+        // run json and display photo using ajax shorthand
+        $.getJSON(app.flickrAPI, app.flickrQuery, app.displayPhoto);
     });
-
 };
 
 // run the app
