1d5a7e7b12aea51e5866c28cf8cbf5fdb40aa951
dana rosamund teagle (she/they) <95252661+torontopubliclibra@users.noreply.github.com>
2022-07-21 07:24:41 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,21 @@
 $(`#hamburger-menu`).on(`click`, function(){
-    $(`#nav-links`).toggle();
+    if ($(`#hamburger-menu`).hasClass(`hamburger-open`)){
+        $(`#nav-links`).hide();
+        $(`#hamburger-menu i`).css(`transform`, `rotate(0deg)`).css(`transition`, `all 0.5s`);
+        $(`#hamburger-menu`).removeClass(`hamburger-open`)
+    } else {
+        $(`#nav-links`).show();
+        $(`#hamburger-menu i`).css(`transform`, `rotate(90deg)`).css(`transition`, `all 0.5s`);
+        $(`#hamburger-menu`).addClass(`hamburger-open`)
+    }
+})
+window.onresize = (function(){
+    const height = $(window).height();
+    const width = $(window).width();
+    if (height >= width){
+        $(`#hamburger-menu i`).css(`transform`, `none`);
+        $(`#nav-links`).hide();
+    } else {
+        $(`#nav-links`).show();
+    }
 })
